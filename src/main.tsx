edfe5fd729870836a8a405bf1677f448fb2346d7
Movses5555 <mosisrayelyan@gmail.com>
2025-04-06 16:13:33 +0400

    asd

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import Index from './pages/index.tsx'
 import './index.css'
 
 const rootElement = document.getElementById("root")!;
 const root = createRoot(rootElement);
 
 // Render the React application
-root.render(<App />);
+root.render(<Index />);
